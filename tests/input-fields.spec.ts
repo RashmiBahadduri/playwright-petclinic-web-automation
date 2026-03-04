a15982f2b4f654da5a20ba637a6aac2180885dfc
Rashmi Mavrck <rashmi@mavrck.co>
2026-03-04 13:51:41 -0500

    modified the pr by incorpoarting the changes recommended

--- tests/input-fields.spec.ts
+++ tests/input-fields.spec.ts
@@ -1,24 +1,47 @@
-import { test, expect } from "../fixtures/page-object-fixture";
+import { test, expect } from "@playwright/test";
 
-test.describe("Input fields tests", () => {
-  test("Update pet type", async ({ pettypes, page }) => {
-    let petName: string[] = ["rabbit", "cat"];
-
-    await pettypes.navigateToPettypes();
-
-    await pettypes.editPettype(petName);
+test.describe(" Pet Types Input fields tests", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/");
+    await page.getByTitle("pettypes").click();
+    await expect(page.getByRole("heading")).toHaveText("Pet Types");
   });
 
-  test("Cancel pet type update", async ({ pettypes, page }) => {
-    await pettypes.navigateToPettypes();
-    await pettypes.cancelEditPetType();
+  test("Update pet type", async ({ page }) => {
+    const firstPetEditButton = page.getByRole("button", { name: "Edit" }).first();
+    await firstPetEditButton.click();
+    await expect(page.getByRole("heading")).toHaveText("Edit Pet Type");
+    const editPetTypeInput = page.locator("#name");
+    await expect(editPetTypeInput).toHaveValue("cat");
+    await editPetTypeInput.fill("rabbit");
+    await page.getByRole("button", { name: "Update" }).click();
+    const firstPetTypeTextBox = page.getByRole("textbox").first();
+    await expect(firstPetTypeTextBox).toHaveValue("rabbit");
+    await firstPetEditButton.click();
+    await expect(editPetTypeInput).toHaveValue("rabbit");
+    await editPetTypeInput.fill("cat");
+    await page.getByRole("button", { name: "Update" }).click();
+    await expect(firstPetTypeTextBox).toHaveValue("cat");
   });
 
-  test("Validtaion of Pet Type Name is required", async ({
-    pettypes,
-    page,
-  }) => {
-    await pettypes.navigateToPettypes();
-    await pettypes.validatePetNameIsRequired();
+  test("Cancel pet type update", async ({ page }) => {
+    await page.getByRole("button", { name: "Edit" }).nth(1).click();
+    const editPetTypeInput = page.locator("#name");
+    await expect(editPetTypeInput).toHaveValue("dog");
+    await editPetTypeInput.fill("moose");
+    await expect(editPetTypeInput).toHaveValue("moose");
+    await page.getByRole("button", { name: "Cancel" }).click();
+    await expect(page.getByRole("textbox").nth(1)).toHaveValue("dog");
+  });
+
+  test("Validation of Pet Type Name is required", async ({ page }) => {
+    await page.getByRole("button", { name: "Edit" }).nth(2).click();
+    const editPetTypeInput = page.locator("#name");
+    await expect(editPetTypeInput).toHaveValue("lizard");
+    await editPetTypeInput.clear();
+    await expect(page.getByText("Name is required")).toHaveText("Name is required");
+    await page.getByRole("button", { name: "Cancel" }).click();
+    await expect(page.getByRole("heading")).toHaveText("Pet Types");
+    await expect(page.getByRole("textbox").nth(2)).toHaveValue("lizard");
   });
 });
