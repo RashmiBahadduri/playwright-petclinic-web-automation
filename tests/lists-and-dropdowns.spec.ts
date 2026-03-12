a64a706910844345f09783b59e2b2502c6291048
Rashmi Mavrck <rashmi@mavrck.co>
2026-03-12 11:14:51 -0400

    incorporated changes form PR review

--- tests/lists-and-dropdowns.spec.ts
+++ tests/lists-and-dropdowns.spec.ts
@@ -8,49 +8,41 @@ test.describe("automate lists and dropdown fields", () => {
     await expect(page.getByRole("heading")).toHaveText("Owners");
   });
 
-  test("Validate selected pet types from the list", async ({ page }) => {
-    await page.locator('.ownerFullName [href*="/owners/"]').first().click();
+  test.only("Validate selected pet types from the list", async ({ page }) => {
+    await page.getByRole("link", { name: "George Franklin" }).click();
     await expect(page.locator(".ownerFullName")).toHaveText("George Franklin");
-    await page.getByRole("button", { name: "Edit Pet" }).click();
+    await page.locator("app-pet-list", { hasText: "Leo" }).getByRole("button", { name: "Edit Pet" }).click();
     await expect(page.getByRole("heading")).toHaveText("Pet");
     await expect(page.locator("#owner_name")).toHaveValue("George Franklin");
-    const petTypeField = page.locator('select[name="pettype"]');
+    const petTypeField = page.getByLabel("Type");
     await expect(petTypeField).toHaveValue("cat");
-    await petTypeField.click();
     const types = await petTypeField.locator("option").allTextContents();
     for (const type of types) {
       await petTypeField.selectOption(type);
-      await expect(petTypeField).toHaveValue(type);
+      await expect(page.locator("#type1")).toHaveValue(type);
     }
-
-    /*const count= await petTypeField.locator('select option').all();
-    for (const cnt of count){
-        const type = await cnt.textContent();
-        await petTypeField.selectOption(type);
-        await expect(petTypeField).toHaveValue(type);
-    }
-        */
   });
 
   test("Validate the pet type update", async ({ page }) => {
-    await page.locator('.ownerFullName [href*="/owners/"]').nth(2).click();
-    const editPetButton = page.getByRole("button", { name: "Edit Pet" }).last();
+    await page.getByRole("link", { name: "Eduardo Rodriquez" }).click();
+    const rosySection = page.locator("app-pet-list", { hasText: "Rosy" });
+    const editPetButton = rosySection.getByRole("button", { name: "Edit Pet" });
     await editPetButton.click();
     await expect(page.locator("#name")).toHaveValue("Rosy");
-    const petTypeField = page.locator('select[name="pettype"]');
-    await expect(petTypeField).toHaveValue("dog");
-    await petTypeField.selectOption("bird");
-    await expect(petTypeField).toHaveValue("bird");
-    await expect(page.locator("#type1")).toHaveValue("bird");
-    await page.getByRole("button", { name: "Update Pet" }).click();
-    await expect(page.locator("dd").last()).toHaveText("bird");
+    const petTypeOptionField = page.getByLabel("Type");
+    const petType = page.locator("#type1");
+    const updatePetType = page.getByRole("button", { name: "Update Pet" });
+    await expect(petType).toHaveValue("dog");
+    await petTypeOptionField.selectOption("bird");
+    await expect(petType).toHaveValue("bird");
+    await updatePetType.click();
+    await expect(rosySection.locator("dd").nth(2)).toHaveText("bird");
     await editPetButton.click();
     await expect(page.locator("#name")).toHaveValue("Rosy");
-    await expect(petTypeField).toHaveValue("bird");
-    await petTypeField.selectOption("dog");
-    await expect(petTypeField).toHaveValue("dog");
-    await expect(page.locator("#type1")).toHaveValue("dog");
-    await page.getByRole("button", { name: "Update Pet" }).click();
-    await expect(page.locator("dd").last()).toHaveText("dog");
+    await expect(petType).toHaveValue("bird");
+    await petTypeOptionField.selectOption("dog");
+    await expect(petType).toHaveValue("dog");
+    await updatePetType.click();
+    await expect(rosySection.locator("dd").nth(2)).toHaveText("dog");
   });
 });
