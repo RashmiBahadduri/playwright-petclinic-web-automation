2fe41489672f4bf22a028117e41d0129c27b83b9
Rashmi Mavrck <rashmi@mavrck.co>
2026-03-18 22:22:30 -0400

    incorporated changes requested

--- tests/dialog-boxes.spec.ts
+++ tests/dialog-boxes.spec.ts
@@ -7,18 +7,19 @@ test.describe("Dialog Boxes tests", () => {
     await expect(page.getByRole("heading")).toHaveText("Pet Types");
     await page.getByRole("button", { name: "Add" }).click();
     await expect(page.locator("app-pettype-add").getByRole("heading")).toHaveText("New Pet Type");
-    await expect(page.locator("label")).toHaveText("Name");
+    await expect(page.locator("app-pettype-add label")).toHaveText("Name");
     await expect(page.locator("app-pettype-add").getByRole("textbox")).toBeVisible();
     await page.locator("app-pettype-add").getByRole("textbox").fill("pig");
     await page.getByText("Save").click();
-    const newPetRow = page.locator("tr").filter({ hasText: "EditDelete" }).last();
+    const newPetRow = page.locator("table tr").last();
     await expect(newPetRow.getByRole("textbox")).toHaveValue("pig");
     page.on("dialog", async (dialog) => {
-      console.log(dialog.message());
       expect(dialog.message()).toEqual("Delete the pet type?");
       dialog.accept();
     });
     await newPetRow.getByRole("button", { name: "Delete" }).click();
+    //await page.waitForRequest("https://petclinic-api.bondaracademy.com/petclinic/api/pettypes/*");
+    await page.waitForResponse("https://petclinic-api.bondaracademy.com/petclinic/api/pettypes/*");
     await expect(newPetRow.getByRole("textbox")).not.toHaveValue("pig");
   });
 });
