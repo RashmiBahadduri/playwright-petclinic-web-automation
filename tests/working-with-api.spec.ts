67aff35d7b4c061c441fa092878b9e2b6ded5deb
Rashmi Mavrck <rashmi@mavrck.co>
2026-05-18 10:04:06 -0400

    pr-intercept

--- tests/working-with-api.spec.ts
+++ tests/working-with-api.spec.ts
@@ -8,6 +8,56 @@ test.beforeEach(async ({ page }) => {
       body: JSON.stringify(ownerData),
     });
   });
+
+  await page.route("*/**/api/vets", async (route) => {
+    const response = await route.fetch();
+    const responsebody = await response.json();
+    ((responsebody[5].specialties = [
+      {
+        id: 4633,
+        name: "dentistry",
+      },
+      {
+        id: 4632,
+        name: "surgery",
+      },
+      {
+        id: 4631,
+        name: "radiology",
+      },
+      {
+        id: 4634,
+        name: "massage therapy",
+      },
+      {
+        id: 4635,
+        name: "dermatology",
+      },
+      {
+        id: 4636,
+        name: "vaccination",
+      },
+      {
+        id: 4637,
+        name: "pre surgery",
+      },
+      {
+        id: 4638,
+        name: "post surgery",
+      },
+      {
+        id: 4639,
+        name: "emergency care",
+      },
+      {
+        id: 4640,
+        name: "dental care",
+      },
+    ]),
+      await route.fulfill({
+        body: JSON.stringify(responsebody),
+      }));
+  });
   await page.goto("/");
   await page.getByRole("button", { name: "Owners" }).click();
   await page.getByText("SEARCH").click();
@@ -37,4 +87,11 @@ test("validate owners list and visit list count", async ({ page }) => {
   const lunaPetSection = page.locator("app-pet-list", { hasText: "Luna" });
   await expect(lunaPetSection.getByRole("row", { name: "Name" })).toContainText(ownerFirstPet!);
   await expect(lunaPetSection.locator("app-visit-list table > tr")).toHaveCount(10);
+});
+
+test("validate veterinarian specialties", async ({ page }) => {
+  await page.getByRole("button", { name: "Veterinarians" }).click();
+  await page.getByText("ALL").click();
+  const sharonRow = page.getByRole("row", { name: "Sharon Jenkins" });
+  await expect(sharonRow.locator("td").nth(1).locator("div")).toHaveCount(10);
 });
